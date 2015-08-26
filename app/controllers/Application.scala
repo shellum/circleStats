7a170e8b3f4823072d07b0d65f21e252194d9930
Cameron Shellum <shellum@gmail.com>
2015-08-26 00:18:41 -0600

    Create barebones review submission flow

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,38 @@
 package controllers
 
-import play.api._
+import play.api.data._
+import play.api.data.Forms._
 import play.api.mvc._
 
 class Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index())
   }
 
+  def setupReview = Action {
+    Ok(views.html.setupReview())
+  }
+
+  def getHash = Action { implicit request =>
+    val formData = userForm.bindFromRequest.get
+    Ok(formData.name + "23f4se5g")
+  }
+
+  def review(hash: String) = Action {
+    Ok(views.html.review(hash))
+  }
+
+  def save(hash: String) = Action { request =>
+    Ok(views.html.save())
+  }
+
+  val userForm = Form(
+    mapping(
+      "name" -> text
+    )(User.apply)(User.unapply)
+  )
+
 }
+
+case class User(name: String)
